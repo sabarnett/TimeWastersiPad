b409ba4a6cd545381f20901d3f9e3cc3bc2eb023
Steven barnett <steve@sabarnett.co.uk>
2025-08-28 21:19:55 +0100

    Fill the triangle in minesweeper - it looks odd not filled.

--- MineSweeper/MineSweeper/SquareView.swift
+++ MineSweeper/MineSweeper/SquareView.swift
@@ -52,7 +52,7 @@ struct SquareView: View {
                         .font(.system(size: textSize))
                 }
             } else if square.isFlagged {
-                Image(systemName: "exclamationmark.triangle")
+                Image(systemName: "exclamationmark.triangle.fill")
                     .foregroundStyle(.black, .yellow)
                     .shadow(color: .black, radius: 3)
                     .font(.system(size: textSize))
