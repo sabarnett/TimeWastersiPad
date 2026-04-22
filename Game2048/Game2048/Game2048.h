5466018be61c86aa8b6caf7afe2abdc713e0c774
Steven barnett <steve@sabarnett.co.uk>
2026-04-22 13:25:23 +0100

    Resolve issue with mossing unbrella header file.

---
+++ Game2048/Game2048/Game2048.h
@@ -0,0 +1,24 @@
+//
+// -----------------------------------------
+// Original project: Game2048
+// Original package: Game2048
+// Created on: 22/04/2026 by: Steven Barnett
+// Web: http://www.sabarnett.co.uk
+// GitHub: https://www.github.com/sabarnett
+// -----------------------------------------
+// Copyright © 2026 Steven Barnett. All rights reserved.
+//
+
+// Game2048.h
+// Game2048 Framework Umbrella Header
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Game2048.
+FOUNDATION_EXPORT double Game2048VersionNumber;
+
+//! Project version string for Game2048.
+FOUNDATION_EXPORT const unsigned char Game2048VersionString[];
+
+// Import all public headers below:
+// #import <Game2048/SomePublicClass.h>
