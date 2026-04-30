a6698b5c694548d8fb48a1db3a2cd6e69536522a
Steven barnett <steve@sabarnett.co.uk>
2026-04-30 13:43:01 +0100

    Fix wirdcraft file length warnings

--- WordCraft/WordCraft/GamePlay/WordCraftViewModel.swift
+++ WordCraft/WordCraft/GamePlay/WordCraftViewModel.swift
@@ -15,7 +15,7 @@ import SharedComponents
 class WordCraftViewModel {
 
     @ObservationIgnored
-    private var wordcraftPlaySounds = true {
+    var wordcraftPlaySounds = true {
         didSet {
             UserDefaults.standard.set(wordcraftPlaySounds, forKey: Constants.wordcraftPlaySounds)
             updateSounds()
@@ -23,7 +23,7 @@ class WordCraftViewModel {
     }
 
     @ObservationIgnored
-    private var cancellables = Set<AnyCancellable>()
+    var cancellables = Set<AnyCancellable>()
 
     var columns = [[Tile]]()
     var notifyMessage: ToastConfig?
@@ -43,6 +43,11 @@ class WordCraftViewModel {
     private var targetLength = 0
 
     var currentRule: Rule!
+
+    var sounds: AVAudioPlayer!
+    var tileDrop: AVAudioPlayer!
+    var backgroundURL: URL { soundFile(named: "background") }
+    var splatURL: URL { soundFile(named: "splat") }
 
     let dictionary = Dictionary(size: .large).filtered(wordMinLength: 2, wordMaxLength: 13)
 
@@ -226,14 +231,6 @@ class WordCraftViewModel {
 
         return count
     }
-
-    // MARK: - Souond variables
-
-    private var sounds: AVAudioPlayer!
-    private var tileDrop: AVAudioPlayer!
-    private var backgroundURL: URL { soundFile(named: "background") }
-    private var splatURL: URL { soundFile(named: "splat") }
-
 }
 
 // MARK: - Rule processing
@@ -329,68 +326,6 @@ extension WordCraftViewModel {
             selectRule()
         } else {
             currentRule = newRule
-        }
-    }
-}
-
-// MARK: - Sound related functions
-extension WordCraftViewModel {
-
-    /// Play the background music
-    func playBackgroundSound() {
-        playSound(backgroundURL, repeating: true)
-    }
-
-    /// If the background music is playing, stop it.
-    func stopSounds() {
-        if sounds != nil {
-            sounds.stop()
-        }
-    }
-
-    /// Play the tile drop sound while the new tiles enter into the game play area. This
-    /// will play over the top of the background sound.
-    func playTileDropSound() {
-        guard wordcraftPlaySounds else { return }
-        tileDrop = try? AVAudioPlayer(contentsOf: splatURL)
-        tileDrop.play()
-    }
-
-    /// Toggle the playing of sounds. If toggled off, the current sound is stopped. If
-    /// toggled on, then we start playing the ticking sound. It is unlikely that we were playing
-    /// any other sound, so this is a safe bet.
-    func toggleSounds() {
-        wordcraftPlaySounds.toggle()
-    }
-
-    private func updateSounds() {
-        speakerIcon = wordcraftPlaySounds ? "speaker.slash" : "speaker"
-
-        if wordcraftPlaySounds {
-            playSound(backgroundURL, repeating: true)
-        } else {
-            sounds.stop()
-        }
-    }
-
-    /// Creates the URL of a sound file. The file must exist within the minesweeper project
-    /// bundle.
-    private func soundFile(named file: String) -> URL {
-        let bun = Bundle(for: WordCraftViewModel.self)
-        let sound = bun.path(forResource: file, ofType: "mp3")
-        return URL(fileURLWithPath: sound!)
-    }
-
-    /// Play a sound file. We will be passed the URL of the file in the current bundle. If sounds are
-    /// disabled, we do nothing.
-    private func playSound(_ url: URL, repeating: Bool = false) {
-        guard wordcraftPlaySounds else { return }
-        if sounds != nil { sounds.stop() }
-
-        sounds = try? AVAudioPlayer(contentsOf: url)
-        if sounds != nil {
-            sounds.numberOfLoops = repeating ? -1 : 0
-            self.sounds.play()
         }
     }
 }
